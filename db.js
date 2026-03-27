0a2270d485443e500f9d7bb0b6ed0fcd5934bc46
Mukesh Jha <developer.mukeshjha@gmail.com>
2026-03-27 12:03:51 +0530

    Databse set in db.js file

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'apna_bazar',
+    user: process.env.DB_USER || 'u102065690_apna_bazar',
+    password: process.env.DB_PASSWORD || 'ApnaBazar@123',
+    database: process.env.DB_NAME || 'u102065690_apna_bazar',
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
